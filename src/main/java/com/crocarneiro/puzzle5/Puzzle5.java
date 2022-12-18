ddeffa05fd911e66788520744a9d590a2f1fbef6
Carlos Carneiro <contato@carlos.carneiro.nom.br>
2022-12-18 03:59:43 -0300

    WIP - Day 5

--- src/main/java/com/crocarneiro/puzzle5/Puzzle5.java
+++ src/main/java/com/crocarneiro/puzzle5/Puzzle5.java
@@ -3,7 +3,11 @@ package com.crocarneiro.puzzle5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Puzzle5 {
@@ -13,13 +17,28 @@ public class Puzzle5 {
         try(var inputStream = Puzzle5.class.getResourceAsStream("testInput.txt")) {
             var bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
-            String line;
-            while((line = bufferedReader.readLine()) != null) {
+            var linesStream = bufferedReader.lines();
+            var linesList = linesStream.toList();
+            bufferedReader.close();
+
+            List<String> crates = new ArrayList<>();
+            linesList.stream().forEach(line -> {
                 var matcher = cratePattern.matcher(line);
-                while(matcher.find()) {
-                    System.out.println(matcher.group() + " ");
-                }
-            }
+                if(matcher.find())
+                    crates.add(line);
+            });
+            Collections.reverse(crates);
+
+            crates.stream().forEach(System.out::println);
+
+//            String line;
+//            while((line = bufferedReader.readLine()) != null) {
+//                var matcher = cratePattern.matcher(line);
+//
+//                if(matcher.find()) {
+//                    System.out.println(line);
+//                }
+//            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
